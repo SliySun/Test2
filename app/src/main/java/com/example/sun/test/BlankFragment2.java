bc1647ba8530460989d43152bc8c87f54c2664d2
SliySun <806928336@qq.com>
2016-12-20 17:27:02 +0800

    0.1

---
+++ app/src/main/java/com/example/sun/test/BlankFragment2.java
@@ -0,0 +1,157 @@
+package com.example.sun.test;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+//import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link BlankFragment2.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link BlankFragment2#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BlankFragment2 extends Fragment implements View.OnClickListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public BlankFragment2() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BlankFragment2.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BlankFragment2 newInstance(String param1, String param2) {
+        BlankFragment2 fragment = new BlankFragment2();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_blank_fragment2, container, false);
+        view.findViewById(R.id.button).setOnClickListener(this);
+        view.findViewById(R.id.button2).setOnClickListener(this);
+        return view;
+    }
+//
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) getActivity();
+//            System.out.println(mListener);
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+
+//
+//    @Override
+//    public void onAttach(Activity activity) {
+//        super.onAttach(activity);
+//        mListener= (OnFragmentInteractionListener) activity;
+//    }
+
+    @Override
+    public void onAttach(Context context) {
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement ABC_Listener");
+        }
+        super.onAttach(context);
+    }
+
+    //SDK API<23时，onAttach(Context)不执行，需要使用onAttach(Activity)。Fragment自身的Bug，v4的没有此问题
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            if (activity instanceof OnFragmentInteractionListener) {
+                mListener = (OnFragmentInteractionListener) activity;
+            } else {
+                throw new RuntimeException(activity.toString()
+                        + " must implement ABC_Listener");
+            }
+        }
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                mListener.changeValue("1");
+//                Toast.makeText(getActivity(),"+++++"+mListener,Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2:
+                mListener.changeValue("2");
+                break;
+        }
+
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+//        void onFragmentInteraction(Uri uri);
+        void changeValue(String value);
+    }
+}
--- app/src/main/java/com/example/sun/test/MainActivity.java
+++ app/src/main/java/com/example/sun/test/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.sun.test;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BlankFragment2.OnFragmentInteractionListener{
 
+    private BlankFragment2 fragment;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView= (TextView) findViewById(R.id.textView_helloworld);
+        addContentLayout();
+    }
+
+    private void addContentLayout() {
+        FragmentManager manger=getFragmentManager();
+        FragmentTransaction transaction=manger.beginTransaction();
+        fragment=BlankFragment2.newInstance(null,null);
+        transaction.add(R.id.fragment_content,fragment);
+        transaction.commit();
+    }
+
+    @Override
+    public void changeValue(String value) {
+        textView.setText(value);
     }
 }
