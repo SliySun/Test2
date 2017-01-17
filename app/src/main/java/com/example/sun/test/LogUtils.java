ae9c0327a9da100e725e4effb5e905aeb027364c
SliySun <806928336@qq.com>
2017-01-18 05:07:54 +0800

    0.2.7.2

--- app/src/main/java/com/example/sun/test/LogUtils.java
+++ app/src/main/java/com/example/sun/test/LogUtils.java
@@ -8,7 +8,7 @@ import android.util.Log;
 
 public class LogUtils {
 
-    private static boolean enablelog=false;
+    private static boolean enablelog=true;
 
     public static void v(String tag,String msg){
         if (enablelog)
