524322ff02484e534f1f94f25ddbe712950a1903
SliySun <806928336@qq.com>
2017-01-18 03:31:05 +0800

    0.2.7.2

--- app/src/main/java/com/example/sun/test/LogUtils.java
+++ app/src/main/java/com/example/sun/test/LogUtils.java
@@ -1,8 +1,36 @@
 package com.example.sun.test;
 
+import android.util.Log;
+
 /**
  * Created by sun on 2017/1/18.
  */
 
 public class LogUtils {
+
+    private static boolean enablelog=false;
+
+    public static void v(String tag,String msg){
+        if (enablelog)
+        Log.v(tag,msg);
+    }
+    public static void d(String tag,String msg){
+        if (enablelog)
+        Log.d(tag,msg);
+    }
+
+    public static void i(String tag,String msg){
+        if (enablelog)
+        Log.i(tag,msg);
+    }
+
+    public static void w(String tag,String msg){
+        if (enablelog)
+        Log.w(tag,msg);
+    }
+
+    public static void e(String tag,String msg){
+        if (enablelog)
+        Log.e(tag,msg);
+    }
 }
--- app/src/main/java/com/example/sun/test/MainActivity.java
+++ app/src/main/java/com/example/sun/test/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 
@@ -14,10 +15,20 @@ public class MainActivity extends AppCompatActivity implements BlankFragment2.On
 
     private BlankFragment2 fragment;
     private TextView textView;
+
+    private String tag="com.example.sun.test";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //tag：标签; msg ：信息
+        LogUtils.v(tag,"我是v级别");
+        LogUtils.d(tag,"我是d级别");
+        LogUtils.i(tag,"我是i级别");
+        LogUtils.w(tag,"我是w级别");
+        LogUtils.e(tag,"我是e级别");
 
         textView= (TextView) findViewById(R.id.textView_helloworld);
         addContentLayout();
